fb6cffccee61959c728664ac1451c8308b373728
Keenan <keenangolding@hotmail.co.uk>
2018-11-28 23:30:28 +0000

    initial commit - started to add first component

---
+++ src/app/posts/post-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; // importing component feature - destructured
+
+@Component({ // using component
+  selector: 'app-post-create', // html selector
+  templateUrl: './post-create.component.html' // html url
+  // styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+
+}
