f60a4a8117dac951d97b5f8576bb725c69e6438c
Keenan-PG.Keenan <kgolding8991@hotmail.com>
2018-12-20 11:09:02 +0000

    using ngForm directives & local refs to submit and validate data

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 // using post interface
 import { Post } from '../posts.model';
+import { NgForm } from '@angular/forms';
 
 @Component({ // using component
   selector: 'app-post-create',
@@ -16,11 +17,14 @@ export class PostCreateComponent {
   // output decorator makes event accessible to outside component
   @Output() postCreated = new EventEmitter<Post>(); // creating new event to emit - of type Post
 
-  onSubmitPost() {
+  onSubmitPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     // creating post object from submitted post
     const post: Post = { // giving type Post to const obj
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post); // passing in created object as value to emit
   }
